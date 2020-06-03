9d6e8d3fec55859e40012e6138933fa3e2a711dd
WeiVi <wei_vi@outlook.com>
2020-06-04 02:08:50 +0800

    1

--- Api/app/Config.py
+++ Api/app/Config.py
@@ -29,7 +29,7 @@ MAIL_PASSWORD = ''
 
 # ----------------------------------------------------------------------
 
-API_DOC_MEMBER = ['admin','album','anime','auth','bbs','article','user','video']
+API_DOC_MEMBER = ['album','anime','auth','bbs','article','user','video']
 
 # 需要排除的 RESTful Api 文档
 RESTFUL_API_DOC_EXCLUDE = []
